2c31bca1da8d01f9c0c7d7e3d27905cd82426de6
Burak Seyhan <seyhanburak9@gmail.com>
2020-01-02 15:11:04 +1100

    refactor code

--- app.js
+++ app.js
@@ -2,9 +2,10 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
-let items = ["Buy Food", "Cook Food", "Eat Food"];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
 let workItems = [];
 
 app.set("view engine", "ejs");
@@ -12,21 +13,13 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req, res) {
-  let today = new Date();
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
-
+  const day = date.getDay();
   res.render("list", { listTitle: day, newListItem: items });
 });
 
 app.post("/", function(req, res) {
   console.log(req.body);
-  let item = req.body.newItem;
+  const item = req.body.newItem;
   if (req.body.list === "Work") {
     workItems.push(item);
     res.redirect("/work");
