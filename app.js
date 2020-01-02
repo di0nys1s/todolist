23221932a7c4c6c1b21dd0bb2be0302de9d664b9
Burak Seyhan <seyhanburak9@gmail.com>
2020-01-02 14:40:01 +1100

    adding layouts and about page

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -20,13 +21,33 @@ app.get("/", function(req, res) {
 
   let day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { kindOfDay: day, newListItem: items });
+  res.render("list", { listTitle: day, newListItem: items });
 });
 
 app.post("/", function(req, res) {
+  console.log(req.body);
   let item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function(req, res) {
+  res.render("list", { listTitle: "Work List", newListItem: workItems });
+});
+
+app.get("/about", function(req, res) {
+  res.render("about");
+});
+
+app.post("/work", function(req, res) {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
 });
 
 app.listen(3000, function() {
