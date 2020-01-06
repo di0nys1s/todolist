7dd50b7f5b426e2c563351b05dc495729e6b944f
Burak Seyhan <seyhanburak9@gmail.com>
2020-01-06 17:23:25 +1100

    heroku connection changes

--- app.js
+++ app.js
@@ -151,6 +151,12 @@ app.post("/work", function(req, res) {
   res.redirect("/work");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = proces.env.PORT;
+if (port === null || port == "") {
+  port = 3000;
+}
+app.listen(port);
+
+app.listen(port, function() {
+  console.log("Server has started successfully");
 });
