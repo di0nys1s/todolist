59b32117cd956a38fb098c3b127e108efe5890b1
Burak Seyhan <seyhanburak9@gmail.com>
2020-01-06 18:54:40 +1100

    port fix remove unnecessary code

--- app.js
+++ app.js
@@ -147,8 +147,7 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
-app.listen(port);
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server has started successfully at port 3000");
 });
